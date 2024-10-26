9d6b7ee94191d73d17fea4a998bbb0621ae541fa
Jaxon Adams <jadamsresume1@gmail.com>
2024-10-26 12:27:16 -0600

    Set up React Router for routing to different pages
    Currently I've only set up the home page and a default error page. The
    next page I'll set up is a login view.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider
+} from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import NotFound from './pages/notFound.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <NotFound />,
+    }
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
