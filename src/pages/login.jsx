a48dc5fa60646f093821660ee78b37aaeac86b0d
Jaxon Adams <jadamsresume1@gmail.com>
2024-10-26 12:47:04 -0600

    Add initial login form with some styles
    I'm using bootstrap for most of my styling currently. The login process
    I built only requires username (email) and password, so these are the
    only two fields I'll be needing.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,19 @@
 const Login = () => {
     return (
         <div id="login-page">
-            <div className="container">
-                Hello from the login page!
+            <div className="container my-3 p-3 border border-dark rounded">
+                <form id="login-form">
+                    <div className="mb-3">
+                        <label htmlFor="input-username" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="input-username" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="input-password" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="input-password" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
     );
