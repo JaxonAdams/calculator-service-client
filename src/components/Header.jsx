d8247917d45b110d15fa395f1af494164f575731
Jaxon Adams <jaxon.adams@loanpro.io>
2024-11-05 19:48:13 -0700

    Flesh out landing page and add balance display
    I've added a few changes to make the landing page for the site look
    nicer.
    
    Additionally, I've added a "current balance" display to the Header
    component which fetches the user's balance from the API.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,50 @@ import { useNavigate } from "react-router-dom";
 import JWTService from "../services/JWTService";
 import iconSvg from "../assets/icon.svg";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const Header = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [balance, setBalance] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
         setIsLoggedIn(JWTService.isLoggedIn());
     }, []);
 
+    useEffect(() => {
+        if (isLoggedIn) {
+            fetchUserBalance();
+        } else {
+            setBalance(null);
+        }
+    }, [isLoggedIn]);
+
+    const fetchUserBalance = async () => {
+        try {
+            const response = await fetch(`${API_BASE_URL}/api/v1/users/balance`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${JWTService.getToken()}`,
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch balance");
+            }
+
+            const data = await response.json();
+            setBalance(parseFloat(data.balance));
+        } catch (error) {
+            console.error("Failed to fetch balance:", error);
+        }
+    }
+
     const handleLogout = () => {
         JWTService.removeToken();
         setIsLoggedIn(false);
-        navigate("/login");
+        navigate("/");
     };
 
     return (
@@ -30,11 +62,20 @@ const Header = () => {
                     />
                     Premium Calculator
                 </a>
-                {isLoggedIn ? (
-                    <button onClick={handleLogout} className="btn btn-outline-secondary" type="button">Logout</button>
-                ) : (
-                    <a href="/login" className="btn btn-primary">Login</a>
-                )}
+                <div className="d-flex align-items-center">
+                    {isLoggedIn ? (
+                        <>
+                            <button onClick={handleLogout} className="btn btn-outline-secondary" type="button">Logout</button>
+                            {balance && 
+                                <p className="p-2 px-4 mb-0 ms-5 bg-success-subtle rounded-start-pill fw-medium" style={{"letterSpacing": 1.3}}>
+                                    {balance.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+                                </p>
+                            }
+                        </>
+                    ) : (
+                        <a href="/login" className="btn btn-primary">Login</a>
+                    )}
+                </div>
             </div>
         </nav>
     );
