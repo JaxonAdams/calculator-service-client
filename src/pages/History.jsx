bcd0ff0eb905ce6dbdf3073e391c0e515bb528b5
Jaxon Adams <jaxon.adams@loanpro.io>
2024-11-06 11:58:25 -0700

    Populate table with first page of user history
    I've added some light formatting to the data, but I plan on adding more
    soon.

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -27,11 +27,65 @@ const History = () => {
         console.log(history["results"]);
     };
 
+    const formatDateStr = (dateStr) => {
+        const date = new Date(dateStr);
+        return date.toLocaleString([], { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' });
+    };
+
+    const formatOperands = (operands) => {
+        let formatted = [];
+        for (let i = 0; i < operands.length; i++) {
+            let operand = operands[i];
+
+            if (typeof operand === "number") {
+                formatted.push(operand);
+                continue;
+            }
+
+            // if operand type is object-literal, format it as a json string
+            if (typeof operand === "object") {
+                // I may come back later to include the object in a nice format
+                formatted.push("-")
+                continue;
+            }
+        }
+
+        return formatted.join(", ");
+    };
+
+    const formatBalance = (balance) => {
+        return parseFloat(balance).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    };
+
     return (
         <div id="history-page">
             <Header />
             <div className="container my-3 p-3 border border-dark rounded">
-                <h1>History</h1>
+                <h1 className="p-3 mb-5 border-bottom border-3">History</h1>
+                <table className="table table-striped table-hover rounded">
+                    <thead className="table-dark">
+                        <tr>
+                            <th scope="col">Date</th>
+                            <th scope="col">Operation</th>
+                            <th scope="col">Operands</th>
+                            <th scope="col">Result</th>
+                            <th scope="col">Remaining Balance</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {calcHistory.map((calculation, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{formatDateStr(calculation["date"])}</td>
+                                    <td>{calculation["operation"]["type"]}</td>
+                                    <td>{formatOperands(calculation["calculation"]["operands"])}</td>
+                                    <td>{calculation["calculation"]["result"]}</td>
+                                    <td>{formatBalance(calculation["user_balance"])}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
