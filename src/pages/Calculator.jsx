8f173dafeb82d95308746718ce871a87abe45325
Jaxon Adams <jaxon.adams@loanpro.io>
2024-11-11 16:47:52 -0700

    Add conditional rendering of input for operands
    In order to account for different operations having different operand
    counts allowed (e.g. addition allows any number, square root requires
    one), I've set up this way of conditionally adding operand inputs to the
    form if the user wants to plug another number in.

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -8,6 +8,8 @@ import CalculatorAPIService from "../services/CalculatorAPIService";
 const Calculator = () => {
     const [availableOps, setAvailableOps] = useState([]);
     const [selectedOp, setSelectedOp] = useState(null);
+    const [additionalOperands, setAdditionalOperands] = useState([]);
+    const [operandValues, setOperandValues] = useState([]);
 
     useEffect(() => {
         fetchAvailableOps();
@@ -26,24 +28,87 @@ const Calculator = () => {
     const handleOpSelect = (opType) => {
         const userSelectedOp = availableOps.find(op => op.type === opType);
         setSelectedOp(userSelectedOp);
+
+        if (userSelectedOp.options.operand_count !== "variable") {
+            setAdditionalOperands([]);  // reset additional operands
+
+            if (userSelectedOp.options.operand_type === "number") {
+                setOperandValues([operandValues[0]]);
+            } else {
+                setOperandValues([]);
+            }
+        }
+
+        console.log("Operation options: ", userSelectedOp.options);
+    };
+
+    const handleOperandChange = (e) => {
+        const index = parseInt(e.target.id.split("-")[1]) - 1;
+        const value = e.target.value;
+
+        const newValues = [...operandValues];
+        newValues[index] = value;
+        setOperandValues(newValues);
+    };
+
+    const configureFirstOperand = () => {
+        if (!selectedOp) {
+            return;
+        }
+
+        if (selectedOp.options.operand_type === "number") {
+            return (
+                <div className="input-group mb-3">
+                    <label htmlFor="operand-1" className="input-group-text">Operand 1</label>
+                    <input type="number" className="form-control" id="operand-1" onChange={handleOperandChange} />
+                    {selectedOp.options.operand_count === "variable" && (
+                        <button className="btn btn-outline-secondary" type="button" onClick={addOperand}>Add Operand</button>
+                    )}
+                </div>
+            );
+        }
+    };
+
+    const addOperand = () => {
+        setAdditionalOperands([
+            ...additionalOperands, 
+            <div className="input-group mb-3">
+                <label htmlFor={`operand-${operandValues.length + 1}`} className="input-group-text">Operand {additionalOperands.length + 2}</label>
+                <input type="number" className="form-control" id={`operand-${operandValues.length + 1}`} onChange={handleOperandChange} />
+            </div>
+        ]);
     };
 
     return (
         <div className="calculator">
             <Header updateBalance={false} setUpdateBalance={() => {}} />
             <div id="run-calculation" className="container my-3 p-3 border border-dark rounded">
-                <h2 className="text-center">Run a Calculation</h2>
+                <h2 className="p-3 mb-5 border-bottom border-3">Run a Calculation</h2>
                 <form className="calculation-form">
-                    <label htmlFor="operation-type" className="form-label">Operation Type</label>
-                    <div className="input-group mb-3">
-                        <span className="input-group-text">{selectedOp ? formatCurrency(selectedOp.cost) : "$~"}</span>
-                        <select className="form-select" id="operation-type" onChange={e => handleOpSelect(e.target.value)}>
-                            {availableOps.map((op, index) => (
-                                <option key={index} value={op.type}>{formatOperator(op.type)}</option>
-                            ))}
-                        </select>
+                    
+                    <div className="mb-3">
+                        <label htmlFor="operation-type" className="form-label">Operation Type</label>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">{selectedOp ? formatCurrency(selectedOp.cost) : "$~"}</span>
+                            <select className="form-select" id="operation-type" onChange={e => handleOpSelect(e.target.value)}>
+                                {availableOps.map((op, index) => (
+                                    <option key={index} value={op.type}>{formatOperator(op.type)}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="form-text">{selectedOp && selectedOp.options.description}</div>
+                    </div>
+
+                    <div className="mb-3">
+                        {configureFirstOperand()}
+                        {additionalOperands.map((operand, index) => (
+                            <div key={index}>
+                                {operand}
+                            </div>
+                        ))}
                     </div>
                 </form>
+                <div>TESTING... {operandValues.join(", ")}</div>
             </div>
         </div>
     );
